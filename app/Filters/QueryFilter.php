683080f04770e7f4bc2864c1c60fed42e14d374e
fedebacci <federicobacci1999@gmail.com>
2025-10-29 10:59:53 +0100

    added a limit filter

--- app/Filters/QueryFilter.php
+++ app/Filters/QueryFilter.php
@@ -36,6 +36,16 @@ class QueryFilter
             $query->whereNotNull('image');
         }
 
+        if ($request->filled('limit')) {
+            $query->limit($request->input('limit'));
+        }
+        
+        // ! For now works more like limit (gives back right number of resources but does not give back pagination links)
+        // todo: differentiate from limit by managing pagination links both here and in results of public's area request
+        // if ($request->filled('paginate')) {
+        //     $query->paginate($request->input('paginate'));
+        // }
+
         return $query;
     }
 }
